4d48aa588ca80a810e97b3684106db3a67a0b1a3
Arthur <arthur393@gmail.com>
2020-05-10 22:05:16 -0300

    Modify vendor to get ascii-art through cURL

--- {{cookiecutter.project_slug}}/egg.sh
+++ {{cookiecutter.project_slug}}/egg.sh
@@ -1,40 +1,26 @@
-prolado(){
+HIDDEN_ART=".ascii-art"
 
-    echo " \$\$\$\$\$\$\  \$\$\      \$\$\   \$\$\  \$\$\$\$\$\$\        \$\$\   \$\$\  \$\$\$\$\$\$\  \$\$\      \$\$\ \$\$\$\$\$\$\$\$\ "
-    echo "\$\$  __\$\$\ \$\$ |     \$\$ |  \$\$ |\$\$  __\$\$\       \$\$\$\  \$\$ |\$\$  __\$\$\ \$\$\$\    \$\$\$ |\$\$  _____|"
-    echo "\$\$ /  \__|\$\$ |     \$\$ |  \$\$ |\$\$ /  \__|      \$\$\$\$\ \$\$ |\$\$ /  \$\$ |\$\$\$\$\  \$\$\$\$ |\$\$ |      "
-    echo "\\$\$\$\$\$\$\  \$\$ |     \$\$ |  \$\$ |\$\$ |\$\$\$\$\       \$\$ \$\$\\$\$ |\$\$\$\$\$\$\$\$ |\$\$\\$\$\\$\$ \$\$ |\$\$\$\$\$\    "
-    echo " \____\$\$\ \$\$ |     \$\$ |  \$\$ |\$\$ |\_\$\$ |      \$\$ \\$\$\$\$ |\$\$  __\$\$ |\$\$ \\$\$\$  \$\$ |\$\$  __|   "
-    echo "\$\$\   \$\$ |\$\$ |     \$\$ |  \$\$ |\$\$ |  \$\$ |      \$\$ |\\$\$\$ |\$\$ |  \$\$ |\$\$ |\\$  /\$\$ |\$\$ |      "
-    echo "\\$\$\$\$\$\$  |\$\$\$\$\$\$\$\$\\\$\$\$\$\$\$  |\\$\$\$\$\$\$  |      \$\$ | \\$\$ |\$\$ |  \$\$ |\$\$ | \_/ \$\$ |\$\$\$\$\$\$\$\$\ "
-    echo " \______/ \________|\______/  \______/       \__|  \__|\__|  \__|\__|     \__|\________|"
-    echo
+fetch_ascii_art(){
+    touch $(HIDDEN_ART);
+    curl \
+    'https://ssfy.sh/dev/text-to-ascii-art@d9d0510c/textToAsciiArt?text={{cookiecutter.project_slug}}&font=5 Line Oblique' >> $(HIDDEN_ART)
+    echo "" >> $(HIDDEN_ART)
+    echo "By: https://github.com/arthurhenrique/cookiecutter-fastapi" >> $(HIDDEN_ART)
+}
+
+prolado(){
+    cat "$(HIDDEN_ART)"
     echo "[RUNNING] http://localhost:8080/docs"
     echo "[CTRL-C] to exit or wait.."
-    echo
-    echo "TODO: apply this feature to get dynamic and apply according {{cookiecutter.project_slug}}"
-
-    
 }
-    
 
 prooutro(){
-
-    echo "  /\$\$\$\$\$\$  /\$\$       /\$\$   /\$\$  /\$\$\$\$\$\$        /\$\$   /\$\$  /\$\$\$\$\$\$  /\$\$      /\$\$ /\$\$\$\$\$\$\$\$"
-    echo " /\$\$__  \$\$| \$\$      | \$\$  | \$\$ /\$\$__  \$\$      | \$\$\$ | \$\$ /\$\$__  \$\$| \$\$\$    /\$\$\$| \$\$_____/"
-    echo "| \$\$  \__/| \$\$      | \$\$  | \$\$| \$\$  \__/      | \$\$\$\$| \$\$| \$\$  \ \$\$| \$\$\$\$  /\$\$\$\$| \$\$      "
-    echo "|  \$\$\$\$\$\$ | \$\$      | \$\$  | \$\$| \$\$ /\$\$\$\$      | \$\$ \$\$ \$\$| \$\$\$\$\$\$\$\$| \$\$ \$\$/\$\$ \$\$| \$\$\$\$\$   "
-    echo " \____  \$\$| \$\$      | \$\$  | \$\$| \$\$|_  \$\$      | \$\$  \$\$\$\$| \$\$__  \$\$| \$\$  \$\$\$| \$\$| \$\$__/   "
-    echo " /\$\$  \ \$\$| \$\$      | \$\$  | \$\$| \$\$  \ \$\$      | \$\$\  \$\$\$| \$\$  | \$\$| \$\$\  \$ | \$\$| \$\$      "
-    echo "|  \$\$\$\$\$\$/| \$\$\$\$\$\$\$\$|  \$\$\$\$\$\$/|  \$\$\$\$\$\$/      | \$\$ \  \$\$| \$\$  | \$\$| \$\$ \/  | \$\$| \$\$\$\$\$\$\$\$"
-    echo " \______/ |________/ \______/  \______/       |__/  \__/|__/  |__/|__/     |__/|________/"
-    echo
+    cat "$(HIDDEN_ART)"
     echo "[RUNNING] http://localhost:8080/docs"
     echo "[CTRL-C] to exit or wait..."
-    echo
-    echo "TODO: apply this feature to get dynamic and apply according {{cookiecutter.project_slug}}"
 }
 
-# Change on: http://patorjk.com/software/taag/#p=display&f=Big%20Money-nw&t={{cookiecutter.project_slug}}
+
+fetch_ascii_art
 egg="$(python -c 'print("clear ; prolado ; sleep 1 ; clear ; prooutro; sleep 1;" * (0xDEADBEAF - (0xDEADBEAF - 42)))')"
 eval $egg

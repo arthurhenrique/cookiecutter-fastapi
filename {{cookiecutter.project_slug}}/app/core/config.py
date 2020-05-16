94c67111ee6a9b342258d84495da2d46ec100ac3
Arthur <arthur393@gmail.com>
2020-05-16 16:21:06 -0300

    Update configurations

--- {{cookiecutter.project_slug}}/app/core/config.py
+++ {{cookiecutter.project_slug}}/app/core/config.py
@@ -11,13 +11,13 @@ from app.core.logging import InterceptHandler
 config = Config(".env")
 
 API_PREFIX = "/api"
-VERSION = "{{cookiecutter.version}}"
+VERSION = "0.1.0"
 DEBUG: bool = config("DEBUG", cast=bool, default=False)
 MAX_CONNECTIONS_COUNT: int = config("MAX_CONNECTIONS_COUNT", cast=int, default=10)
 MIN_CONNECTIONS_COUNT: int = config("MIN_CONNECTIONS_COUNT", cast=int, default=10)
-SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret)
+SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret, default="")
 
-PROJECT_NAME: str = config("PROJECT_NAME", default="{{cookiecutter.project_name}}")
+PROJECT_NAME: str = config("PROJECT_NAME", default="Name of the project")
 
 # logging configuration
 LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
@@ -26,5 +26,5 @@ logging.basicConfig(
 )
 logger.configure(handlers=[{"sink": sys.stderr, "level": LOGGING_LEVEL}])
 
-MODEL_PATH = config("MODEL_PATH", default="/opt/ml/model")
+MODEL_PATH = config("MODEL_PATH", default=None)
 MODEL_NAME = config("MODEL_NAME", default=None)

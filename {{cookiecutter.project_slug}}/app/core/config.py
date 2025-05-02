6439e1859e6aadfa60679f85d60d10e447331fd1
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2025-05-02 11:49:19 -0300

    fix: missing config for startup (#158)
    * fix: run startup
    
    * missing config
    
    * missing config
    
    * missing version config

--- {{cookiecutter.project_slug}}/app/core/config.py
+++ {{cookiecutter.project_slug}}/app/core/config.py
@@ -9,14 +9,14 @@ from starlette.datastructures import Secret
 config = Config(".env")
 
 API_PREFIX = "/api"
-VERSION = "0.1.0"
+VERSION = "{{cookiecutter.version}}"
 DEBUG: bool = config("DEBUG", cast=bool, default=False)
 MAX_CONNECTIONS_COUNT: int = config("MAX_CONNECTIONS_COUNT", cast=int, default=10)
 MIN_CONNECTIONS_COUNT: int = config("MIN_CONNECTIONS_COUNT", cast=int, default=10)
 SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret, default="")
 MEMOIZATION_FLAG: bool = config("MEMOIZATION_FLAG", cast=bool, default=True)
 
-PROJECT_NAME: str = config("PROJECT_NAME", default="manu")
+PROJECT_NAME: str = config("PROJECT_NAME", default="{{cookiecutter.project_name}}")
 
 # logging configuration
 LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
@@ -25,8 +25,6 @@ logging.basicConfig(
 )
 logger.configure(handlers=[{"sink": sys.stderr, "level": LOGGING_LEVEL}])
 
-MODEL_PATH = config(
-    "MODEL_PATH", default="/Users/arthur.dasilva/repos/arthurhenrique/n"
-)
-MODEL_NAME = config("MODEL_NAME", default="pregnancy_model_local.joblib")
-INPUT_EXAMPLE = config("INPUT_EXAMPLE", default="./ml/model/examples/example.json")
+MODEL_PATH = config("MODEL_PATH", default="{{cookiecutter.machine_learn_model_path}}")
+MODEL_NAME = config("MODEL_NAME", default="{{cookiecutter.machine_learn_model_name}}")
+INPUT_EXAMPLE = config("INPUT_EXAMPLE", default="{{cookiecutter.input_example_path}}")
